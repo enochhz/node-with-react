b6b8236c0e935e5f095513ca4861917c2b4908e2
walkerzheng <9771330@qq.com>
2017-09-27 13:10:14 -0700

    test

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ passport.use(
         }
     )
 );
-
+https://accounts.google.com/o/oauth2/v2/auth?
+// response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauth%2Fgoogle%2Fcallback&scope=profile%20email&client_id=230288455038-c45spub9n119t8te07jf44odhk38sh8h.apps.googleusercontent.com
 app.get(
     '/auth/google',
     passport.authenticate('google', {
