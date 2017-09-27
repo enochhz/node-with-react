efbce712a9815765e83ae0b073fadad73c199f03
walkerzheng <walkerhaozheng@gmail.com>
2017-09-27 15:58:48 -0700

    3-10 Passport Callbacks done()

--- services/passport.js
+++ services/passport.js
@@ -1,32 +1,39 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const keys = require("./../config/keys");
 
-const Users = mongoose.model('users');
+const Users = mongoose.model("users");
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: "/auth/google/callback"
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log('\n================== After Google OAuth =======================');
-            console.log('access token ', accessToken);
-            console.log('refreshToken ', refreshToken);
-            console.log('profile', profile);
-            console.log('================== Done ======================================\n');
-            Users.findOne({ googleId: profile.id}).then((existingUser) => {
-               if (existingUser)  {
-                   // we already have a record with the given profile ID
-                   console.log('Old user login');
-               } else {
-                   new Users({ googleId : profile.id}).save(); // save() put the model instance into mongodb collection
-                   console.log('A new User login!\nsuccess: new User created.');
-               }
-            })
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(
+        "\n================== After Google OAuth ======================="
+      );
+      console.log("access token ", accessToken);
+      console.log("refreshToken ", refreshToken);
+      console.log("profile", profile);
+      console.log(
+        "================== Done ======================================\n"
+      );
+      Users.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          console.log("Old user login");
+          done(nul, existingUser); // first argu: err is null, second argu: exist user
+        } else {
+          new Users({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user)); // save() put the model instance into mongodb collection
+          console.log("A new User login!\nsuccess: new User created.");
         }
-    )
+      });
+    }
+  )
 );
