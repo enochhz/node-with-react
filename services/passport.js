cb0eb35065ea583323752fff57259110d4e4943f
walkerzheng <walkerhaozheng@gmail.com>
2017-09-27 15:36:05 -0700

    3-8 Mongoose Model Classes

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require('mongoose');
 const keys = require("./../config/keys");
+
+const Users = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy(
@@ -10,9 +13,13 @@ passport.use(
             callbackURL: "/auth/google/callback"
         },
         (accessToken, refreshToken, profile, done) => {
+            console.log('\n================== After Google OAuth =======================');
             console.log('access token ', accessToken);
             console.log('refreshToken ', refreshToken);
             console.log('profile', profile);
+            console.log('================== Done ======================================\n');
+            new Users({ googleId : profile.id}).save(); // save() put the model instance into mongodb collection
+            console.log('success: new User created.')
         }
     )
 );
