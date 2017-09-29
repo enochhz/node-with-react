b6dc9884e8030844630f9280b035e2df1ac669d1
walkerzheng <walkerhaozheng@gmail.com>
2017-09-29 13:59:38 -0700

    fixed proxy

--- services/passport.js
+++ services/passport.js
@@ -20,8 +20,9 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-      proxy: true
+      // callbackURL: "/auth/google/callback",
+      // proxy: true
+      callbackURL: keys.callbackURL
     },
     (accessToken, refreshToken, profile, done) => {
       Users.findOne({ googleId: profile.id }).then(existingUser => {
