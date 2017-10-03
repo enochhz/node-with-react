a239e230a0d2b875dbf6cfcc47029e5c60a532cb
walkerzheng <walkerhaozheng@gmail.com>
2017-10-02 17:18:11 -0700

    create header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <nav>
+      <nav className="blue darken-2">
         <div className="nav-wrapper">
-          <a className="brand-logo">Emily</a>
+          <a className="brand-logo center">Emily</a>
           <ul className="right">
             <li><a>Login With Google</a></li>
           </ul>
