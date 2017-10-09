b17eacf34b29814fafd07946f25b96e1c38eaae4
walkerzheng <walkerhaozheng@gmail.com>
2017-10-09 16:03:53 -0700

    update button look

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -3,13 +3,18 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
   render() {
-    debugger;
     return (
       <StripeCheckout
+        name="Emaily"
+        description="$5 from email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn red darken-1">
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
