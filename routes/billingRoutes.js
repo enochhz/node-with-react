3cf27ecfb8e901d0f292d18ed251b277610cc3d1
walkerzheng <walkerhaozheng@gmail.com>
2017-10-11 16:53:49 -0700

    update post stripe

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -4,13 +4,14 @@ const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
   app.post('/api/stripe', requireLogin, async (req, res) => {
-    const charge = await stripe.charges.create({
-      amount: 500,
-      currency: 'usd',
-      description: '$5 for 5 credits',
-      source: req.body.id
-    });
-    req.user.credit += 5;
+    // following stripes payment api doesn't not work so far
+    // const charge = await stripe.charges.create({
+    //   amount: 500,
+    //   currency: 'usd',
+    //   description: '$5 for 5 credits',
+    //   source: req.body.id
+    // });
+    req.user.credits += 5;
     const user = await req.user.save();
     res.send(user);
   });
