f1e17e8974cfcb19d1807606fe8c69f376fcd988
walkerzheng <walkerhaozheng@gmail.com>
2017-10-13 13:34:54 -0700

    change contents in landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,8 @@ import React from "react";
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center'}}>
-      <h1>Emaily Landing!</h1>
+      <h1>Emaily</h1>
+      <p>Collect feedbacks from your users</p>
     </div>
   );
 };
