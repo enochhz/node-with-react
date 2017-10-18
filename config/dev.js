16f1f027c22e0a6508754e612da42e1b13de848d
walkerzheng <walkerhaozheng@gmail.com>
2017-10-18 15:29:11 -0700

    10-4 Creating Subdoc Collections

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js - don't commit this!!!
+module.exports = {
+    googleClientID:'230288455038-c45spub9n119t8te07jf44odhk38sh8h.apps.googleusercontent.com',
+    googleClientSecret: 'aqWJTGtaYl6xiSXxoYmn2pyV',
+    mongoURL: 'mongodb://walkerzheng:Zh19950513@ds155424.mlab.com:55424/emaily-dev',
+    cookieKey: 'cookiekeysecret',
+    stripePublishableKey: 'pk_test_20AhL3c0KK40yV8TNuNwiuEp',
+    stripeSecretKey: 'sk_test_Bt46bfv3GMgmuf1Pg3O46F5q'
+};
