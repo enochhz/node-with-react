972aa94b85557a9942f5a38c2eb3a37e2930c72c
walkerzheng <walkerhaozheng@gmail.com>
2017-10-25 15:50:09 -0700

    finish webhoooks routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const surveyTemplate = require("./../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
+  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for your response!");
   });
 
@@ -30,6 +30,19 @@ module.exports = app => {
       })
       .compact()
       .uniqBy(compactEvents, "email", "surveyId")
+      .each(({ surveyId, email, choice })=> {
+        // update new survey in database
+        Survey.findOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false}
+          }
+        }, {
+          $inc: { [choice]: 1},
+          $set: { 'recipients.$.responded': true},
+          lastResponded: new Date()
+        }).exec();
+      })
       .value();
     console.log(events);
   });
